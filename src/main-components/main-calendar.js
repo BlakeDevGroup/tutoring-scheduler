d3faea1508256a864f95dd28daccafbb3640c91b
Gabriel Blake <61571088+gabrielblake28@users.noreply.github.com>
2021-07-31 04:08:31 -0400

    refactored main calendar, added data/events
    refactored main calendar component from app.js. created temporary store for events files under data/events.json

---
+++ src/main-components/main-calendar.js
@@ -0,0 +1,43 @@
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import React, { useState } from "react";
+
+
+import Events from "../models/events";
+import eventsData from "../data/events.json";
+
+const getEvents = () => {
+
+    let events = []
+
+    eventsData["events"].forEach(data => {
+        
+        events.push(data)
+    });
+    
+    
+
+    return events
+}
+const MainCalendar = (props) => {
+    const [events, setEvents] = useState(getEvents())
+    
+
+    return (
+        <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        initialView="timeGridWeek"
+        expandRows={true}
+        handleWindowResize
+        selectable={true}
+        navLinks={true}
+        events={events}
+        />
+    );
+}
+    
+
+
+export default MainCalendar;
