17d4eddfbb24df73799d6e36c005458d2f51266d
Gabriel Blake <61571088+gabrielblake28@users.noreply.github.com>
2021-08-02 08:04:07 -0400

    Update events.js
    added more event props

--- src/models/events.js
+++ src/models/events.js
@@ -5,6 +5,18 @@ export default class Events {
         this.title = eventObject.title
         this.start = eventObject.start
         this.end = eventObject.end
+        this.editable = eventObject.editable
+        this.display = eventObject.display
+        this.color = eventObject.color
+        this.startRecur = eventObject.startRecur
+        this.endRecur = eventObject.endRecur
+        this.textColor = eventObject.textColor
+        this.backgroundColor = eventObject.backgroundColor
+        this.eventResizableFromStart= eventObject.eventResizableFromStart
+        this.eventDurationEditable = eventObject.eventDurationEditable
+        this.eventOverlap = eventObject.eventOverlap
+        this.eventAllow = eventObject.eventAllow
+        this.color = eventObject.color
     }
 
 
