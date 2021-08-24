27296edf59fac6115e7f0732577e3c22a9f79833
cdblake1 <cdblake31@gmail.com>
2021-08-24 02:20:04 -0400

    fixed discrepancy

--- src/Create-events/Company-drop-menu.js
+++ src/Create-events/Company-drop-menu.js
@@ -4,33 +4,30 @@ import React, { useState } from "react";
 import companyData from "../data/companies.json";
 
 const getCompanies = () => {
+  let companies = [];
 
-  let companies = []
-
-  companyData["companies"].forEach(data => {
-      
-      companies.push(data["companyName"])
+  companyData["companies"].forEach((data) => {
+    companies.push(data["companyName"]);
   });
-  console.log(companies)
-  return companies
-}
-
+  console.log(companies);
+  return companies;
+};
 
 function CompanyDropMenu(props) {
-    const [value, setValue] = React.useState("medium");
-    const [company, setsetCompanies] = useState(getCompanies())
+  const [value, setValue] = React.useState("medium");
+  const [company, setsetCompanies] = useState(getCompanies());
 
-    return (
-      <Select
-        icon={<CaretDownFill />}
-        size="small"
-        options={company}
-        value={props.value}
-        placeholder="Select company"
-        onChange={(e) => props.onChange(e.target.value)}
-        multiple={false}
-      />
-    );
-  }
+  return (
+    <Select
+      icon={<CaretDownFill />}
+      size="small"
+      options={company}
+      value={props.value}
+      placeholder="Select company"
+      onChange={(e) => props.onChange(e.target.value)}
+      multiple={false}
+    />
+  );
+}
 
 export default CompanyDropMenu;
