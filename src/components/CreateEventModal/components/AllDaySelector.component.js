2c04655b0e1719296a61be58538c4bef95686617
Gabriel Blake <gabriel.blake28@gmail.com>
2021-09-01 13:49:19 -0600

    Added a selectable drop down menu when "recurring" checkbox is checked

--- src/components/CreateEventModal/components/AllDaySelector.component.js
+++ src/components/CreateEventModal/components/AllDaySelector.component.js
@@ -1,12 +1,29 @@
-import { CheckBox, Box, Layer, Select } from "grommet";
+import { CheckBox, Box, Layer, Select, Button } from "grommet";
 import React from "react";
 // import RecuringDateSelectPopup from "./RecurringDateSelectModal.component";
 
+const Option = React.memo(({ value, selected }) => (
+  <Box direction="row" gap="small" align="center" pad="xsmall">
+    <CheckBox tabIndex="-1" checked={selected} onChange={() => {}} />
+    {value}
+  </Box>
+));
+
+const dummyOptions = Array(7)
+  .fill()
+  .map((_, i) => `option ${i}`)
+  .sort((a, b) =>
+    a.localeCompare(b, undefined, { numeric: true, sensitivity: "base" })
+  );
+
 export default function AllDaySelector(props) {
   const [allDayChecked, setAllDayChecked] = React.useState(false);
-  const [recurringChecked, setrecurringChecked] = React.useState(false);
+  const [recurringChecked, setRecurringChecked] = React.useState(false);
   const [show, setShow] = React.useState();
   const [value, setValue] = React.useState();
+  const [selected, setSelected] = React.useState([]);
+  const [options, setOptions] = React.useState(dummyOptions);
+  console.log(value);
   return (
     <Box
       margin={{
@@ -33,14 +50,8 @@ export default function AllDaySelector(props) {
           onEsc={() => setShow(false)}
           onClickOutside={() => setShow(false)}
         >
-          <Box
-            margin={{
-              top: "large",
-              left: "large",
-              right: "large",
-              bottom: "large",
-            }}
-          >
+          <Box fill align="center" justify="start" pad="large">
+            <Button margin={{bottom:"small"}} label="save" onClick={() => setShow(false)} />
             <Select
               options={[
                 "Sunday",
@@ -51,9 +62,42 @@ export default function AllDaySelector(props) {
                 "Friday",
                 "Saturday",
               ]}
+              multiple={true}
+              placeholder="select an option..."
+              closeOnChange={false}
               value={value}
-              onChange={() => setShow(false)}
-            />
+              onChange={({ option }) => setValue(option)}
+              size="medium"
+              onClose={() =>
+                setOptions(
+                  options.sort((p1, p2) => {
+                    const p1Exists = selected.includes(p1);
+                    const p2Exists = selected.includes(p2);
+
+                    if (!p1Exists && p2Exists) {
+                      return 1;
+                    }
+                    if (p1Exists && !p2Exists) {
+                      return -1;
+                    }
+                    return p1.localeCompare(p2, undefined, {
+                      numeric: true,
+                      sensitivity: "base",
+                    });
+                  })
+                )
+              }
+              onChange={({ selected: nextSelected }) => {
+                setSelected(nextSelected);
+              }}
+            >
+              {(option, index) => (
+                <Option
+                  value={option}
+                  selected={selected.indexOf(index) !== -1}
+                />
+              )}
+            </Select>
           </Box>
         </Layer>
       )}
