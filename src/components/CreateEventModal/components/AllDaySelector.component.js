f29b16e7965f6a805e34fdcb4879de7f8bc2d830
Gabriel Blake <gabriel.blake28@gmail.com>
2021-09-01 13:18:57 -0600

    Added a popup where you can select the date you want your event to recur on

--- src/components/CreateEventModal/components/AllDaySelector.component.js
+++ src/components/CreateEventModal/components/AllDaySelector.component.js
@@ -1,9 +1,12 @@
-import { CheckBox, Box } from "grommet";
+import { CheckBox, Box, Layer, Select } from "grommet";
 import React from "react";
+// import RecuringDateSelectPopup from "./RecurringDateSelectModal.component";
 
-export default function AllDaySelector() {
-  const [allDayChecked, setAllDayChecked] = React.useState(true);
-  const [recurringChecked, setrecurringChecked] = React.useState(true);
+export default function AllDaySelector(props) {
+  const [allDayChecked, setAllDayChecked] = React.useState(false);
+  const [recurringChecked, setrecurringChecked] = React.useState(false);
+  const [show, setShow] = React.useState();
+  const [value, setValue] = React.useState();
   return (
     <Box
       margin={{
@@ -20,12 +23,40 @@ export default function AllDaySelector() {
         onChange={(event) => setAllDayChecked(event.target.checked)}
         pad="small"
       />
-
       <CheckBox
         checked={recurringChecked}
         label="recurring"
-        onChange={(event) => setrecurringChecked(event.target.checked)}
+        onChange={() => setShow(true)}
       />
+      {show && (
+        <Layer
+          onEsc={() => setShow(false)}
+          onClickOutside={() => setShow(false)}
+        >
+          <Box
+            margin={{
+              top: "large",
+              left: "large",
+              right: "large",
+              bottom: "large",
+            }}
+          >
+            <Select
+              options={[
+                "Sunday",
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+              ]}
+              value={value}
+              onChange={() => setShow(false)}
+            />
+          </Box>
+        </Layer>
+      )}
     </Box>
   );
 }
